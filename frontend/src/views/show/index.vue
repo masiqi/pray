3eed595720e2569a5aa7f7b0d5459596a089f654
masiqi <masiqi@gmail.com>
2018-10-22 19:07:47 +0800

    显示轮播图

--- frontend/src/views/show/index.vue
+++ frontend/src/views/show/index.vue
@@ -1,96 +1,124 @@
 <template>
   <div class="app-container">
-    <el-row>
-      <el-col :span="24">
-        <div class="grid-content bg-purple-dark" />
-      </el-col>
-    </el-row>
-    <el-row>
-      <el-col :span="12">
-        <div class="grid-content bg-purple" />
-      </el-col>
-      <el-col :span="12">
-        <div class="grid-content bg-purple-light" />
-      </el-col>
-    </el-row>
-    <el-row>
-      <el-col :span="8">
-        <div class="grid-content bg-purple" />
-      </el-col>
-      <el-col :span="8">
-        <div class="grid-content bg-purple-light" />
-      </el-col>
-      <el-col :span="8">
-        <div class="grid-content bg-purple" />
-      </el-col>
-    </el-row>
-    <el-row>
-      <el-col :span="6">
-        <div class="grid-content bg-purple" />
-      </el-col>
-      <el-col :span="6">
-        <div class="grid-content bg-purple-light" />
-      </el-col>
-      <el-col :span="6">
-        <div class="grid-content bg-purple" />
-      </el-col>
-      <el-col :span="6">
-        <div class="grid-content bg-purple-light" />
-      </el-col>
-    </el-row>
-    <el-row>
-      <el-col :span="4">
-        <div class="grid-content bg-purple" />
-      </el-col>
-      <el-col :span="4">
-        <div class="grid-content bg-purple-light" />
-      </el-col>
-      <el-col :span="4">
-        <div class="grid-content bg-purple" />
-      </el-col>
-      <el-col :span="4">
-        <div class="grid-content bg-purple-light" />
-      </el-col>
-      <el-col :span="4">
-        <div class="grid-content bg-purple" />
-      </el-col>
-      <el-col :span="4">
-        <div class="grid-content bg-purple-light" />
-      </el-col>
-    </el-row>
+    <el-carousel :interval="5000"
+                 :height="height"
+                 arrow="always">
+      <el-carousel-item v-for="item in fileList"
+                        :key="item.url">
+        <img :src="item.url"
+             class="img_show">
+      </el-carousel-item>
+    </el-carousel>
   </div>
 </template>
 
 <script>
+import { getSchedule } from '@/api/table'
+// import PrayTimes from 'prayer-times'
+
 export default {
+  data() {
+    return {
+      dialogImageUrl: '',
+      dialogVisible: false,
+      fileList: [],
+      height: '0px',
+      form: {
+        tz: 0,
+        lang: '',
+        lat: '',
+        lon: '',
+        image: '',
+        cm: '',
+        athan: '',
+        jamaah: '',
+        imask_delta: '',
+        fajr_delta: '',
+        sunrise_delta: '',
+        dhuhr_delta: '',
+        athan_delta: '',
+        jamaah_delta: '',
+        asr_delta: '',
+        maghrib_delta: '',
+        isha_delta: '',
+        imask_fixed: '',
+        fajr_fixed: '',
+        sunrise_fixed: '',
+        dhuhr_fixed: '',
+        athan_fixed: '',
+        jamaah_fixed: '',
+        asr_fixed: '',
+        maghrib_fixed: '',
+        isha_fixed: ''
+      }
+    }
+  },
   mounted: function() {
+    this.height = Math.ceil((window.innerHeight * 4) / 5) + 'px'
     console.log(this.$route.params.pk)
+  },
+  created() {
+    this.getDetail()
+  },
+  methods: {
+    getDetail() {
+      this.loading = true
+      getSchedule(this.$route.params.pk).then(response => {
+        console.log(response)
+        this.form.tz = response.data.tz
+        this.form.lang = response.data.lang
+        this.form.lat = response.data.lat
+        this.form.lon = response.data.lon
+        this.form.image = response.data.image
+        this.form.cm = response.data.cm
+        this.form.athan = response.data.athan
+        this.form.jamaah = response.data.jamaah
+        this.form.imask_delta = response.data.imask_delta
+        this.form.fajr_delta = response.data.fajr_delta
+        this.form.sunrise_delta = response.data.sunrise_delta
+        this.form.dhuhr_delta = response.data.dhuhr_delta
+        this.form.asr_delta = response.data.asr_delta
+        this.form.maghrib_delta = response.data.maghrib_delta
+        this.form.isha_delta = response.data.isha_delta
+        this.form.imask_fixed = response.data.imask_fixed
+        this.form.fajr_fixed = response.data.fajr_fixed
+        this.form.sunrise_fixed = response.data.sunrise_fixed
+        this.form.dhuhr_fixed = response.data.dhuhr_fixed
+        this.form.asr_fixed = response.data.asr_fixed
+        this.form.maghrib_fixed = response.data.maghrib_fixed
+        this.form.isha_fixed = response.data.isha_fixed
+        if (response.data.image) {
+          response.data.image.forEach(element => {
+            this.fileList.push({ url: process.env.CDN_URL + element })
+          })
+        }
+      })
+      this.loading = false
+    }
   }
 }
 </script>
 
 <style>
-.el-row {
-  margin-bottom: 20px;
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 18px;
+  opacity: 0.75;
+  line-height: 300px;
+  margin: 0;
 }
-.el-col {
-  border-radius: 4px;
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
 }
-.bg-purple-dark {
-  background: #99a9bf;
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
 }
-.bg-purple {
-  background: #d3dce6;
-}
-.bg-purple-light {
-  background: #e5e9f2;
-}
-.grid-content {
-  border-radius: 4px;
-  min-height: 36px;
-}
-.row-bg {
-  padding: 10px 0;
-  background-color: #f9fafc;
+
+img.img_show {
+  width: 100%;
+  height: 100%;
+  display: block;
 }
 </style>
